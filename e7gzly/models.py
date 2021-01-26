66d8d87c7f3abe03013c767897b782326640ba03
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2021-01-26 11:04:56 +0200

    Add Cities Choices to Admin and User Models

--- e7gzly/models.py
+++ e7gzly/models.py
@@ -36,6 +36,48 @@ TEAMS = (
     ('el entag el harby sc', 'El Entag El Harby SC')
 )
 
+CITIES = (("cairo", "Cairo"),
+          ("alexandria", "Alexandria"),
+          ("gizeh", "Gizeh"),
+          ("shubra el-kheima", "Shubra El-Kheima"),
+          ("port said", "Port Said"),
+          ("suez", "Suez"),
+          ("luxor", "Luxor"),
+          ("al-mansura", "Al-Mansura"),
+          ("el-mahalla el-kubra", "El-Mahalla El-Kubra"),
+          ("tanta", "Tanta"),
+          ("asyut", "Asyut"),
+          ("ismailia", "Ismailia"),
+          ("fayyum", "Fayyum"),
+          ("zagazig", "Zagazig"),
+          ("aswan", "Aswan"),
+          ("damietta", "Damietta"),
+          ("damanhur", "Damanhur"),
+          ("al-minya", "Al-Minya"),
+          ("beni suef", "Beni Suef"),
+          ("qena", "Qena"),
+          ("sohag", "Sohag"),
+          ("hurghada", "Hurghada"),
+          ("6th of october city", "6th of October City"),
+          ("shibin el kom", "Shibin El Kom"),
+          ("banha", "Banha"),
+          ("kafr el-sheikh", "Kafr el-Sheikh"),
+          ("arish", "Arish"),
+          ("mallawi", "Mallawi"),
+          ("10th of ramadan city", "10th of Ramadan City"),
+          ("bilbais", "Bilbais"),
+          ("marsa matruh", "Marsa Matruh"),
+          ("idfu", "Idfu"),
+          ("mit ghamr", "Mit Ghamr"),
+          ("al-hamidiyya", "Al-Hamidiyya"),
+          ("desouk", "Desouk"),
+          ("qalyub", "Qalyub"),
+          ("abu kabir", "Abu Kabir"),
+          ("kafr el-dawwar", "Kafr el-Dawwar"),
+          ("girga", "Girga"),
+          ("akhmim", "Akhmim"),
+          ("matareya", "Matareya"))
+
 
 class Admin(StructuredNode):
     username = StringProperty(required=True, max_length=USERNAME_LEN, unique_index=True)
@@ -45,7 +87,7 @@ class Admin(StructuredNode):
     last_name = StringProperty(required=True)
     birthdate = DateProperty(required=True)
     gender = StringProperty(required=True, choices=GENDERS)
-    city = StringProperty(required=True)
+    city = StringProperty(required=True, choices=CITIES)
     address = StringProperty(required=False)
 
 
@@ -64,7 +106,7 @@ class User(StructuredNode):
     last_name = StringProperty(required=True)
     birthdate = DateProperty(required=True)
     gender = StringProperty(required=True, choices=GENDERS)
-    city = StringProperty(required=True)
+    city = StringProperty(required=True, choices=CITIES)
     address = StringProperty(required=False)
     role = StringProperty(required=True, choices=ROLES)
     reservations = RelationshipTo('Seat', 'RESERVED_A')
