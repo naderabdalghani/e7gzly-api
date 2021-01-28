b36e33560145f937fb7bba70b6f132541a60de04
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2021-01-28 05:55:46 +0200

    Add Id Field to Nodes

--- e7gzly/models.py
+++ e7gzly/models.py
@@ -1,10 +1,13 @@
+import datetime
 from neomodel import StructuredNode, StringProperty, EmailProperty, DateTimeProperty, DateProperty, IntegerProperty, \
     ArrayProperty, RelationshipTo, One, UniqueIdProperty, RelationshipFrom, BooleanProperty
-from .constants import *
+from rest_framework.exceptions import ValidationError
+from .constants import NAME_MAX_LEN, STADIUM_NAME_MAX_LEN, CITIES, GENDERS, TEAMS, ROLES, SEAT_ID_MAX_LEN
 
 
 class Admin(StructuredNode):
-    username = StringProperty(required=True, max_length=USERNAME_LEN, unique_index=True)
+    id = UniqueIdProperty()
+    username = StringProperty(required=True, max_length=NAME_MAX_LEN, unique_index=True)
     email = EmailProperty(required=True, unique_index=True)
     password = StringProperty(required=True)
     first_name = StringProperty(required=True)
@@ -17,13 +20,14 @@ class Admin(StructuredNode):
 
 class Seat(StructuredNode):
     ticket_id = UniqueIdProperty()
-    seat_id = StringProperty(required=True)
+    seat_id = StringProperty(required=True, max_length=SEAT_ID_MAX_LEN)
     match = RelationshipTo('Match', 'FOR', cardinality=One)
     user = RelationshipFrom('User', 'RESERVED_A', cardinality=One)
 
 
 class User(StructuredNode):
-    username = StringProperty(required=True, max_length=USERNAME_LEN, unique_index=True)
+    id = UniqueIdProperty()
+    username = StringProperty(required=True, max_length=NAME_MAX_LEN, unique_index=True)
     email = EmailProperty(required=True, unique_index=True)
     password = StringProperty(required=True)
     first_name = StringProperty(required=True)
@@ -38,17 +42,20 @@ class User(StructuredNode):
 
 
 class Stadium(StructuredNode):
-    name = StringProperty(required=True, max_length=STADIUM_NAME_LEN, unique_index=True)
-    seats = IntegerProperty(required=True)
+    id = UniqueIdProperty()
+    name = StringProperty(required=True, max_length=STADIUM_NAME_MAX_LEN, unique_index=True)
+    capacity = IntegerProperty(required=True)
     vip_seats_per_row = IntegerProperty(required=True)
     vip_rows = IntegerProperty(required=True)
+    matches = RelationshipFrom("Match", "HOSTED_IN")
 
 
 class Match(StructuredNode):
+    id = UniqueIdProperty()
     home_team = StringProperty(required=True, choices=TEAMS)
-    away_team = EmailProperty(required=True, choices=TEAMS)
+    away_team = StringProperty(required=True, choices=TEAMS)
     date = DateTimeProperty(required=True)
-    referee = StringProperty(required=True)
-    linesmen = ArrayProperty(StringProperty(), required=True)
+    referee = StringProperty(required=True, max_length=NAME_MAX_LEN)
+    linesmen = ArrayProperty(StringProperty(max_length=NAME_MAX_LEN), required=True)
     match_venue = RelationshipTo('Stadium', 'HOSTED_IN', cardinality=One)
     seats = RelationshipFrom("Seat", "FOR")
