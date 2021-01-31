7cda6862eeabf6a155b8320e55be9f193d76afe2
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2021-01-31 17:19:20 +0200

    Create Permissions

---
+++ e7gzly/permissions.py
@@ -0,0 +1,57 @@
+from rest_framework.exceptions import NotAuthenticated
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+from e7gzly.models import User
+
+
+class IsReadOnlyRequest(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
+class IsPostRequest(BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "POST"
+
+
+class IsPutRequest(BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "PUT"
+
+
+class IsPatchRequest(BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "PATCH"
+
+
+class IsDeleteRequest(BasePermission):
+    def has_permission(self, request, view):
+        return request.method == "DELETE"
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if type(request.user) is not User:
+            raise NotAuthenticated
+        return request.user.role == "admin"
+
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        if type(request.user) is not User:
+            raise NotAuthenticated
+        return request.user.role == "manager"
+
+
+class IsFan(BasePermission):
+    def has_permission(self, request, view):
+        if type(request.user) is not User:
+            raise NotAuthenticated
+        return request.user.role == "fan"
+
+
+class IsAuthorized(BasePermission):
+    def has_permission(self, request, view):
+        if type(request.user) is not User:
+            raise NotAuthenticated
+        return bool(request.user.authorized)
